69127b3872ce130cb93256dfbacf55b417d83664
Filipe Lopes <filipe.pires.lopes@gmail.com>
2018-07-03 21:30:14 +0100

    Initial commit, project created in android studio

---
+++ app/src/main/java/filipe/pires/lopes/gmail/com/coffeedrop/SplashScreen.kt
@@ -0,0 +1,12 @@
+package filipe.pires.lopes.gmail.com.coffeedrop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
